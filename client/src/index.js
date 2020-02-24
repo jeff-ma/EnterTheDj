60c9dd9481999c26113544d1f0f09edd12f24cff
jeff-ma <jefm206@gmail.com>
2020-02-24 11:12:00 -0800

    update and change fonts

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-// import 'react-id-swiper/src/styles/scss/swiper.scss';
-import 'react-id-swiper/lib/styles/scss/swiper.scss';
+import 'swiper/css/swiper.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
