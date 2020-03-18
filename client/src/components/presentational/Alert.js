b87e389bb1d340f32e9524422ed1d95985fcc8e6
jeff-ma <jefm206@gmail.com>
2020-03-17 20:20:55 -0700

    change alert styling

--- client/src/components/presentational/Alert.js
+++ client/src/components/presentational/Alert.js
@@ -1,16 +1,16 @@
-import React, {useLayoutEffect} from 'react';
-import '../../styles/alert.scss';
+import React, {useLayoutEffect} from "react";
+import "../../styles/alert.scss";
 
 const Alert = ({children, removeAlert}) => {
     useLayoutEffect(() => {
-        let timeout = setTimeout(() => removeAlert(), 5000);
+        const timeout = setTimeout(removeAlert, 5000);
         return () => clearTimeout(timeout);
     },[removeAlert]);
     return(
-        <div className="alert alert-dark fade show" role="alert">
+        <div className="alert fade show" role="alert">
             {children}
             <button type="button" className="close" aria-label="Close" onClick={removeAlert}>
-            <span aria-hidden="true">&times;</span>
+                <span aria-hidden="true">&times;</span>
             </button>
         </div>
     );
