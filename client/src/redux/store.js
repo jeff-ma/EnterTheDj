262fb599079fa6554a8cad0da5d348a972ce722b
jeff-ma <jefm206@gmail.com>
2020-04-01 15:54:59 -0700

    clean up code

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,11 +2,14 @@ import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./reducers/rootReducer";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas/rootSaga";
-import {createLogger} from "redux-logger";
+// import {createLogger} from "redux-logger";
 
 const sagaMiddleware = createSagaMiddleware();
-const logger = createLogger({collapsed: true});
-const middlewares = [sagaMiddleware, logger];
+// const logger = createLogger({collapsed: true});
+const middlewares = [
+    sagaMiddleware, 
+    // logger
+];
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga)
 
