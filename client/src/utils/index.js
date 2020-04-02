9c0136c9a15c1d96c1de48cd5da2fdfce95c139a
jeff-ma <jefm206@gmail.com>
2020-04-02 00:29:35 -0700

    clean up code

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -395,7 +395,7 @@ export const addIsSavedToTracks = async (tracks) => {
 // Format milliseconds into MM:SS
 export const formatDuration = (ms) => {
   const minutes = Math.floor(ms / 60000);
-  const seconds = ((ms % 60000) / 1000).toFixed(0);
+  const seconds = Math.floor((ms % 60000) / 1000);
   return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 };
 
