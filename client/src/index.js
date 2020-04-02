4fc4f395f1302b0acc1db97792b3f9edaa697ecb
jeff-ma <jefm206@gmail.com>
2020-04-01 22:03:54 -0700

    clean up code

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "swiper/css/swiper.css";
-import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/store";
-import {CookiesProvider} from "react-cookie";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <CookiesProvider>
-                <App/>
-            </CookiesProvider>
+            <App/>
         </BrowserRouter>
     </Provider>, 
     document.getElementById("root")
