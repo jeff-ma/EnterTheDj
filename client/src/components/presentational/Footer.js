9316431a84b81e3a36d39968b657e9668294bac9
jeff-ma <jefm206@gmail.com>
2021-07-28 19:01:01 -0700

    update email address

--- client/src/components/presentational/Footer.js
+++ client/src/components/presentational/Footer.js
@@ -14,10 +14,10 @@ const Footer = () => (
             <div>
                 <ul>
                     <li>
-                        <p>Email:</p><p><a href="mailto: JeffMaEmail@gmail.com">JeffMaEmail@gmail.com</a></p>
+                        <p>Email:</p><p><a href="mailto: jeff@jeffma.dev">jeff@jeffma.dev</a></p>
                     </li>
                     <li>
-                        <p>Portfolio:</p><p><a href="https://jeffma.dev/" target="_blank" rel="noopener noreferrer">https://JeffMa.dev</a></p>
+                        <p>Portfolio:</p><p><a href="https://jeffma.dev/" target="_blank" rel="noopener noreferrer">https://jeffma.dev</a></p>
                     </li>
                     <li>
                         <p>Github:</p><p><a href="https://github.com/jeff-ma" target="_blank" rel="noopener noreferrer">https://github.com/jeff-ma</a></p>
@@ -33,7 +33,7 @@ const Footer = () => (
                 </div>
                 <div>
                     <p>Developed by Jeff Ma</p>
-                    <p>Copyright &copy;2020</p>                            
+                    <p>Copyright &copy;2021</p>                            
                 </div>
             </div>
         </section>
